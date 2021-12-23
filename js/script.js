f062c06d42d4efd42df6a22fb0827146d4d92721
Robert Padar <robertpadar@gmail.com>
2021-12-23 12:20:15 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ var newHtml =
         '<link rel="icon" href="assets/PNG/Cards/cardClubsJ.png" type="image/png" />' +
     '</head>' +
     '<body  onload="init()">' +
-        '<canvas id="canvas" style="width:1100px; height:650px"></canvas>' +
+        '<canvas id="canvas" width="1100" height="650"></canvas>' +
     '</body>' +
     '</html>';
 
