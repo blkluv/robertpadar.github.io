8e7d2ae4b1154537e4638bdf267e66368daed1f0
Robert Padar <robertpadar@gmail.com>
2021-12-30 14:30:34 +0000

    Update ctl_utils.js

--- roulette/js/ctl_utils.js
+++ roulette/js/ctl_utils.js
@@ -92,9 +92,12 @@ function sizeHandler() {
 
             canvas.style.width = destW+"px";
             canvas.style.height = destH+"px";
-            
+
             s_iScaleFactor = Math.min(destW / CANVAS_WIDTH, destH / CANVAS_HEIGHT);
-            s_oStage.scaleX = s_oStage.scaleY = s_iScaleFactor; 
+            s_oStage.scaleX = s_oStage.scaleY = s_iScaleFactor;
+            
+            $("#canvas").css("width",destW+"px");
+            $("#canvas").css("height",destH+"px");
         }
 		
 		$("#canvas").css("left",((w / 2) - (destW / 2))+"px");
