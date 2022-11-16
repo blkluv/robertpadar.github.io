6469445e3fd9b04b6c67586091afaa386e433a9a
Robert Padar <87752792+robertpadar@users.noreply.github.com>
2022-11-16 17:22:24 +0000

    Update connection.php

--- login/connection.php
+++ login/connection.php
@@ -1,8 +1,8 @@
 <?php
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$dbname = "login_db";
+$dbhost = "sql102.epizy.com";
+$dbuser = "epiz_31468791";
+$dbpass = "GzvFhH2YbnhnU";
+$dbname = "epiz_31468791_casino_hell";
 
 if(!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname)) {
     die("failed to connect!");
