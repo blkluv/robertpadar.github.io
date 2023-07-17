dd36df9687aaccc36ed0a30fa6f1ed8926ed8d18
Robert Padar <87752792+robertpadar@users.noreply.github.com>
2023-07-17 21:21:46 +0100

    deleted

--- README.md
+++
@@ -1,2 +0,0 @@
-# robertpadar.github.io
-demo casino website
--- poker/static/js/WinScreen.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const WinScreen = () => <div> You Win! </div>
-
-export default WinScreen;
-
-
-// WEBPACK FOOTER //
-// ./src/WinScreen.js
