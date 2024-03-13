1aa1ecfacd822d8543e3a1e0a4fec6115daa6885
Robert Padar <87752792+robertpadar@users.noreply.github.com>
2024-03-13 18:55:03 +0000

    Update getCountryCities.php

--- php/getCountryCities.php
+++ php/getCountryCities.php
@@ -1,37 +1,38 @@
 <?php
     
-    
     ini_set('display_errors', 'On');
     error_reporting(E_ALL);
 
     $executionStartTime = microtime(true);
 
-    $country = $_REQUEST['country'];
+    $iso3 = $_REQUEST['country'];
+    
+    $curl = curl_init();
 
-    $url='https://countriesnow.space/api/v0.1/countries/population/cities/filter/q?limit=60&order=asc&orderBy=name&country=' . $country;
-
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_URL, $url);
-
-    $result=curl_exec($ch);
-
-    curl_close($ch); 
-
-    $decode = json_decode($result, true);
-    $decode = $decode['data'];
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => 'https://api.countrystatecity.in/v1/countries/'. $iso3 .'/cities',
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_HTTPHEADER => array(
+        'X-CSCAPI-KEY: NWVqbEhDS29Tc21xQ0lCSkU3eGVwYWVQQkRSNlJ4TlVpT3lEV1ZqVw=='
+      ),
+    ));
+    
+    $response = curl_exec($curl);
+    
+    curl_close($curl);
+    
+    $decode = json_decode($response, true);
+    
     shuffle($decode);
     
-    $decode = array_slice($decode, 0, 20);
-
+    $decode = array_slice($decode, 0, 40);
+    
     $output['status']['code'] = "200";
     $output['status']['name'] = "ok";
     $output['status']['description'] = "success";
     $output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
     $output['data'] = $decode;
-    
-    //header('Content-Type: application/json; charset=UTF-8');
 
     echo json_encode($output);
+
 ?>
